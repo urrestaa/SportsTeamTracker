e379eb55e1d64c41729ce9af071ee2878d7b8462
urrestaa <matias.urrestarazu@stbrendan.edu.uy>
2025-04-01 13:09:11 +0000

    fin

--- player_awards.py
+++ player_awards.py
@@ -1,8 +1,26 @@
 import streamlit as st
 
 def load_player_awards():
-    st.header("Player Awards")
+    st.header("Player of the Month")
 
     st.subheader("Marzo 2025")
 
     st.image("img/Marzo2025.png", caption="Marzo 2025 POTM", width=320)
+
+    st.subheader("Abril 2025")
+
+    st.subheader("Mayo 2025")
+
+    st.subheader("Junio 2025")
+
+    st.subheader("Julio 2025")
+
+    st.subheader("Agosto 2025")
+
+    st.subheader("Septiembre 2025")
+
+    st.subheader("Agosto 2025")
+
+    st.subheader("Noviembre 2025")
+
+    st.header("Player of the Year")
